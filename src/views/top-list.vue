57971bc8d6a463f7e0a98bb2bbadc92648bd2028
李扬 <liyang@liyangdeMacBook-Pro.local>
2022-08-25 23:40:59 +0800

    1⃣1，添加top组件，并添加top上的主页面的跳转。2，修改规则交验文件，不校验name的名称

---
+++ src/views/top-list.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是top-list</div>
+</template>
+
+<script>
+export default {
+    name: 'top-list'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/user-center.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>我是user-center</div>
+</template>
+
+<script>
+export default {
+    name: 'user-center'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
